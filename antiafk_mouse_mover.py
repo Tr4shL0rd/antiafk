82cde0ebf7763fe1483f5d06090f4d5c0a25e125
Tr4shL0rd <ketil04@gmail.com>
2023-01-05 09:50:50 +0100

    linting & type checking

--- antiafk_mouse_mover.py
+++ antiafk_mouse_mover.py
@@ -15,6 +15,7 @@ try:
 except IndexError:
     DEBUG = False
 
+
 def get_current_time() -> str:
     """
     Get the current time in the format [HH:MM:SS | dd/mm/YYYY].
@@ -34,7 +35,8 @@ def get_current_time() -> str:
     time_str = time.strftime("[%H:%M:%S | %d/%m/%Y]")
     return time_str
 
-def split_xy(xy:Point) -> Tuple[float,float]:
+
+def split_xy(xy: Point) -> Tuple[float, float]:
     """
     Split a tuple of x and y coordinates into separate variables.
 
@@ -51,9 +53,10 @@ def split_xy(xy:Point) -> Tuple[float,float]:
     * Does the same as x,y = xy[0], xy[1]
     """
 
-    return (xy[0],xy[1])
+    return (xy[0], xy[1])
 
-def is_near(xy:Point, center_x:int, center_y:int, radius:int) -> bool:
+
+def is_near(xy: Point, center_x: int, center_y: int, radius: int) -> bool:
     """
     Check if the position (x, y) is within a certain distance of the center position (center_x, center_y).
 
@@ -75,12 +78,13 @@ def is_near(xy:Point, center_x:int, center_y:int, radius:int) -> bool:
     """
 
     # Calculate the distance between the given position and the center position
-    x,y = split_xy(xy)
+    x, y = split_xy(xy)
     distance = ((x - center_x) ** 2 + (y - center_y) ** 2) ** 0.5
     # Return whether the distance is less than or equal to the given radius
     return bool(distance <= radius)
 
-def jiggle_mouse(xy:Point) -> None:
+
+def jiggle_mouse(xy: Point) -> None:
     """
     Jiggle the mouse around the given position (x, y).
 
@@ -93,35 +97,47 @@ def jiggle_mouse(xy:Point) -> None:
     * None
     """
 
-    x,y = split_xy(xy)
-    pyautogui.moveTo(x, y+1)
+    x, y = split_xy(xy)
+    pyautogui.moveTo(x, y + 1)
 
-CENTER_LEFT = (1,500)
+
+CENTER_LEFT = (1, 500)
 SLEEP_TIME = 60 if not DEBUG else int(argv[1])
 ORIGIN = pyautogui.position()
 with open("logs/mouse_mover.log", "a") as f:
-    f.write(f"{get_current_time()}: STARTING at position {ORIGIN} |{DEBUG = }|{SLEEP_TIME = }|\n")
+    f.write(
+        f"{get_current_time()}: STARTING at position {ORIGIN} |{DEBUG = }|{SLEEP_TIME = }|\n"
+    )
+
 
 def main() -> None:
     pos = Point(pyautogui.position()[0], pyautogui.position()[1])
     pyautogui.moveTo(CENTER_LEFT)
-    while is_near(pos, CENTER_LEFT[0], CENTER_LEFT[1], radius=10): # checks if mouse is around CENTER_LEFT
+    while is_near(
+        pos, CENTER_LEFT[0], CENTER_LEFT[1], radius=10
+    ):  # checks if mouse is around CENTER_LEFT
         jiggle_mouse(pos)
-        for i in range(SLEEP_TIME,0,-1):
-            print(f"\rmoving mouse in {i} seconds" if i > 9 else f"\rmoving mouse in 0{i} seconds", end="" )
+        for i in range(SLEEP_TIME, 0, -1):
+            print(
+                f"\rmoving mouse in {i} seconds"
+                if i > 9
+                else f"\rmoving mouse in 0{i} seconds",
+                end="",
+            )
             sleep(1)
-        pyautogui.moveTo(pos) # resets mouse pos
+        pyautogui.moveTo(pos)  # resets mouse pos
         with open("logs/mouse_mover.log", "a") as f:
             f.write(f"{get_current_time()}: MOVED TO {split_xy(pos)}\n")
     print("stopped")
-    with open("logs/mouse_mover.log","a") as f:
+    with open("logs/mouse_mover.log", "a") as f:
         f.write(f"{get_current_time()}: STOPPED by moving mouse\n")
+
 
 try:
     if __name__ == "__main__":
         main()
 except KeyboardInterrupt:
-    with open("logs/mouse_mover.log","a") as f:
+    with open("logs/mouse_mover.log", "a") as f:
         f.write(f"{get_current_time()}: STOPPED by KeyboardInterrut\n")
     print("EXITING...")
     exit()
